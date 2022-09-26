4eb9c2c70510e413cf4411ff144c38d0a2f98015
Edoardo Debenedetti <edoardo.debenedetti@inf.ethz.ch>
2022-09-26 11:55:56 +0200

    Update batch job script

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,5 +1,5 @@
 declare -a model_names=("resnet18_32" "wide_resnet28_10" "wide_resnet34_10" "wide_resnet34_20")
-declare -a adv_training_techniques=("pgd" "trades")
+declare -a adv_training_techniques=("pgd")
 declare -a attack_steps=("1" "2" "5" "7" "10")
 
 export DATASET=cifar10
@@ -13,19 +13,26 @@ export OUTPUT_DIR=/p/gpfs1/robustHW
 export MEAN="0.4914 0.4822 0.4465"
 export STD="0.2023 0.1994 0.2010"
 
+# Validation-only params
+export VAL_BATCH_SIZE=2048
+export EPS=8
+export N_GPUS=4
+
 for m in "${model_names[@]}"
 do
   for a in "${adv_training_techniques[@]}"
   do
     for s in "${attack_steps[@]}"
     do
+      EXPERIMENT_DIR=${EXPERIMENT}_${m}_${a}_${s}
       {
         bsub -o logs/${m}_${a}_${s}_train_%J.out -nnodes 1 -W $BTIME -G $BANK \
           python -m torch.distributed.run --nproc_per_node=4 --master_port=6712 \
-          train.py ${DATA_DIR} --config=$BASE_CONFIG --log-wandb --experiment=$EXPERIMENT \
+          train.py ${DATA_DIR} --config=$BASE_CONFIG --experiment=$EXPERIMENT_DIR \
           --mean=$MEAN --std=$STD --model=$m --adv-training=$a --attack-steps=$s \
         && bsub -o logs/${m}_${a}_${s}_aa_%J.out -nnodes 1 -W $BTIME -G $BANK \
-          python validate_robustbench.py --data-dir=$DATA_DIR --model=$m --prune_pct=$p --augmentation=$a --prune_type=$t --prune_strategy=$r --batch_size="200"
+          python validate_robustbench.py --data-dir=$DATA_DIR --model=$m --checkpoint=$EXPERIMENT_DIR/best.pth.tar --batch_size=$VAL_BATCH_SIZE \
+          --eps=$EPS --mean=$MEAN --std=STD --gpus=N_GPUS
       } &
     done
   done
