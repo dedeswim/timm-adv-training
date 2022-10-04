6e9cf0e4d5b35d8a8808254e5d64d34c8b2fe8f9
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2022-10-04 07:08:06 -0700

    Updating chaining script

--- scripts/chainer_main.sh
+++ scripts/chainer_main.sh
@@ -8,41 +8,42 @@ export MAX=3
 # number of nodes, we need to specify here instead of in the submit.sh job
 export NNODES=1
 # train script, should just be a filename. `python $TRAINSCRIPT`
-#export TRAINSCRIPT=chainer_test.py
 export TRAINSCRIPT=$1
 # train config, should be a string as used in the python cmd line argument like "--arg1=5 --arg2=6"
 export TRAINCONFIG="$2"
+echo "TRAINCONFIG=$TRAINCONFIG"
+export EXP_OUT_DIR="$3"
+export EXP_NAME="$4"
 # name of the chain: for logging files and job names
-export CHAINNAME=chain_${TRAINSCRIPT}_config_and_iter_${TRAINCONFIG//[^[:alnum:]]/}
+export CHAINNAME=chain_${TRAINSCRIPT}_config_and_iter_${EXP_NAME}
 # script with specific environment settings for the job
-export STARTSCRIPT=chainer_recursion.sh
+export STARTSCRIPT=scripts/chainer_recursion.sh
 # bank to use for the job allocation
 export BBANK=safeml
 # time limit for the job
-#export BTIME=12:00
-export BTIME=00:01
+export BTIME=12:00
 # outdir of the logs
 export BOUTDIR=logs
 
-repeat=$3
+repeat=$5
 
-if [ $# -lt 1 ]; then
+if [ $# -lt 2 ]; then
   echo "Number of arguments not expected. Exiting.."
   exit 0
 fi
 
-if [ $# -gt 3 ]; then
+if [ $# -gt 5 ]; then
   echo "Number of arguments not expected. Exiting.."
   exit 0
 fi
 
-if [ $# -eq 3 ]; then
+if [ $# -eq 5 ]; then
   # Arguments: sh chainer.sh TRAINSCRIPT TRAINCONFIG repeat
   # if there's an argument to chainer.sh script: sh chainer arg0
   # meaning, there was a job running before this one
   lastcount=$repeat
   currcount=$(expr $lastcount + 1)
-  if [ "$currcount" -gt "$MAX" ]; then
+  if [ "$currcount" -gt $MAX ]; then
     echo "Chained more than $MAX jobs. Stop chaining..."
     exit 0
   fi
@@ -50,10 +51,15 @@ if [ $# -eq 3 ]; then
   name="${CHAINNAME}_${currcount}"
   dep_name="${CHAINNAME}_${lastcount}"
   outputname="${name}_%J"
+  # Add the checkpoint to resume to the training config if we are at the first repeat
+  if [ $repeat -eq 1 ]; then
+    TRAINCONFIG="$TRAINCONFIG --resume $EXP_OUT_DIR/$EXP_NAME/last.pth.tar"
+  fi
+
   CMD="bsub -nnodes $NNODES -alloc_flags ipisolate -W $BTIME -G $BBANK -J $name -outdir $BOUTDIR -oo ${BOUTDIR}/${outputname}.out -w ended(${dep_name})"
 fi
 
-if [ $# -eq 2 ]; then
+if [ $# -eq 4 ]; then
   # Arguments: sh chainer.sh TRAINSCRIPT TRAINCONFIG
   # if there's not an argument to chainer.sh script: sh chainer
   currcount=0
@@ -62,5 +68,5 @@ if [ $# -eq 2 ]; then
   CMD="bsub -nnodes $NNODES -alloc_flags ipisolate -W $BTIME -G $BBANK -J $name -outdir $BOUTDIR -oo ${BOUTDIR}/${outputname}.out"
 fi
 
-echo "chainer main: $CMD sh $STARTSCRIPT $TRAINSCRIPT $2 $currcount"
-$CMD sh $STARTSCRIPT $TRAINSCRIPT "$2" $currcount
+echo "chainer main: $CMD sh $STARTSCRIPT $TRAINSCRIPT \"$TRAINCONFIG\" $currcount"
+$CMD sh $STARTSCRIPT $TRAINSCRIPT "$TRAINCONFIG" $EXP_OUT_DIR $EXP_NAME $currcount
